4ebdcb34bed17f75e6d6ef59fd7f561230f2776e
ru_shalm <ru_shalm@hazimu.com>
2016-04-02 14:53:56 +0900

    fix error handler message

--- app/apis/error_formatter.rb
+++ app/apis/error_formatter.rb
@@ -2,6 +2,7 @@ module RutanAPI
   class ErrorFormatter
     class << self
       def call(message, _backtrace, _options, env)
+        env['rutan_api.message'] = message
         env['api.tilt.layout'] = 'layouts/error'
         Grape::Formatter::Rabl.call(message, env)
       end
--- app/views/layouts/error.rabl
+++ app/views/layouts/error.rabl
@@ -3,7 +3,7 @@ object :response if params[:format] == 'xml'
 node :meta do
   {
     status: status,
-    error: @error
+    error: env['rutan_api.message'],
+    hoge: @message
   }.delete_if { |_, v| v.nil? }
 end
-
